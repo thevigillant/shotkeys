f8747a2ceb0b170f2d874c5a2c4ef5817368b1cb
Bruno Santana Reis <brunosantanareisfc@gmail.com>
2025-11-23 16:15:46 -0300

    commit 0.5.5

--- login.php
+++ login.php
@@ -44,6 +44,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
   <meta charset="utf-8">
   <title>Login - ShotKeys</title> <!-- Título atualizado -->
+  <!-- FavIcon -->
+    <link
+      rel="icon"
+      href="assets/icons/favicon/logo-Shot-Keys.ico"
+      type="image/x-icon"
+    />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Bootstrap CSS (CDN) -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
